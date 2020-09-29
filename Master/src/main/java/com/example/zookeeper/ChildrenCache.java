024f7cbcf00c6e903ff7b96336b2813531d0aedf
LPothabattula <lpothabattula@fanatics.com>
2020-09-30 00:44:46 +0530

    Added changes for mastership changes, master waits for worker changes and Master waits for new tasks to assign

---
+++ Master/src/main/java/com/example/zookeeper/ChildrenCache.java
@@ -0,0 +1,54 @@
+package com.example.zookeeper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChildrenCache {
+    List<String> childrenCache;
+
+    ChildrenCache(){
+        childrenCache = null;
+    }
+
+    ChildrenCache(List<String> childrenCache){
+        this.childrenCache = childrenCache;
+    }
+
+    public List<String> getList(){
+        return childrenCache;
+    }
+
+    public List<String> removeAndSet(List<String> newChildren){
+        List<String> diff = null;
+        if(childrenCache != null){
+            for(String child: childrenCache){
+                if(!newChildren.contains(child)){
+                    if(diff==null){
+                        diff = new ArrayList<>();
+                    }
+                    diff.add(child);
+                }
+            }
+        }
+        this.childrenCache = newChildren;
+        return diff;
+    }
+
+    public List<String> addAndSet(List<String> newTasks){
+        List<String> diff = null;
+        if(childrenCache==null){
+            diff = new ArrayList<>(newTasks);
+        }else{
+            for(String task: newTasks){
+                if (!childrenCache.contains(task)){
+                    if(diff==null){
+                        diff = new ArrayList<>();
+                    }
+                    diff.add(task);
+                }
+            }
+        }
+        this.childrenCache = newTasks;
+        return diff;
+    }
+}
--- Master/src/main/java/com/example/zookeeper/Master.java
+++ Master/src/main/java/com/example/zookeeper/Master.java
@@ -4,10 +4,13 @@ import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 import org.slf4j.*;
 
+import java.io.Closeable;
 import java.io.IOException;
 import java.util.*;
 
-public class Master implements Watcher {
+public class Master implements Watcher, Closeable {
+    enum masterstates {RUNNING, ELECTED, NOTELECTED};
+    private masterstates state = masterstates.RUNNING;
     ZooKeeper zk;
     String hostPort;
     Random random  = new Random();
@@ -26,39 +29,118 @@ public class Master implements Watcher {
         zk.close();
     }
 
-    public Boolean checkMaster() throws KeeperException, InterruptedException {
-        while(true){
-            Stat stat = new Stat();
+    @Override
+    public void close() throws IOException{
+        if(zk != null) {
             try {
-                byte[] data = zk.getData("/master", false, stat);
-                isLeader = new String(data).equals(serverID);
-                return true;
-            }catch (KeeperException.NodeExistsException e){
-                return false;
-            }catch (KeeperException.ConnectionLossException e){
+                zk.close();
+            } catch (InterruptedException e) {
+                LOG.warn("Interrupted while closing zookeeper connection");
             }
         }
     }
 
-    public void runForMaster() throws InterruptedException, KeeperException {
-        while(true) {
-            try {
-                zk.create("/master",
-                        serverID.getBytes(),
-                        ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                        CreateMode.EPHEMERAL);
-                isLeader = Boolean.TRUE;
-                break;
-            } catch (KeeperException.NodeExistsException e) {
-                isLeader = Boolean.FALSE;
-                break;
-            } catch (KeeperException.ConnectionLossException e) {
-
-            }
-            if(checkMaster())
-                break;
-        }
+    void checkMaster() {
+        zk.getData("/master",
+                false,
+                checkMasterCallback,
+                null);
     }
+
+    AsyncCallback.DataCallback checkMasterCallback = new AsyncCallback.DataCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    checkMaster();
+                    break;
+                case NONODE:
+                    runForMaster();
+                    break;
+                case OK:
+                    if(serverID.equals(new String(data))){
+                        state = masterstates.ELECTED;
+                        isLeader = Boolean.TRUE;
+                        // TODO: takeLeadership
+                    }else{
+                        state = masterstates.NOTELECTED;
+                        masterExists();
+                    }
+                default:
+                    LOG.error("Error while reading the data: "+KeeperException.create(KeeperException.Code.get(rc),path));
+            }
+        }
+    };
+
+    void masterExists(){
+        zk.exists("/master",
+                masterExistsWatcher,
+                masterExistsCallback,
+                null
+        );
+    }
+
+    Watcher masterExistsWatcher = new Watcher(){
+        @Override
+        public void process(WatchedEvent e){
+            if(e.getType() == Event.EventType.NodeDeleted){
+                assert "/master".equals(e.getPath());
+                runForMaster();
+            }
+        }
+    };
+
+    AsyncCallback.StatCallback masterExistsCallback = new AsyncCallback.StatCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, Stat stat) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    masterExists();
+                    break;
+                case OK:
+                    if(stat == null){
+                        state = masterstates.RUNNING;
+                        runForMaster();
+                    }
+                    break;
+                default:
+                    checkMaster();
+                    break;
+            }
+        }
+    };
+
+
+    public void runForMaster() {
+        zk.create("/master",
+                serverID.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL,
+                createMasterCallback,
+                null);
+    }
+
+    AsyncCallback.StringCallback createMasterCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    checkMaster();
+                    break;
+                case NODEEXISTS:
+                    state = masterstates.NOTELECTED;
+                    masterExists();
+                    break;
+                case OK:
+                    state = masterstates.ELECTED;
+                    // TODO: takeLeadership
+                    break;
+                default:
+                    state = masterstates.ELECTED;
+                    LOG.error("Something terrible happened: "+KeeperException.create(KeeperException.Code.get(rc),path));
+            }
+        }
+    };
 
     public void bootstrap(){
         createParent("/workers", new byte[0]);
@@ -105,4 +187,183 @@ public class Master implements Watcher {
     public void process(WatchedEvent e){
         System.out.println(e);
     }
+
+    Watcher workerChangeWatcher = new Watcher() {
+        @Override
+        public void process(WatchedEvent watchedEvent) {
+            if(watchedEvent.getType() == Event.EventType.NodeChildrenChanged){
+                assert "/workers".equals(watchedEvent.getPath());
+                getWorkers();
+            }
+        }
+    };
+
+    void getWorkers(){
+        zk.getChildren("/workers",
+                workerChangeWatcher,
+                workerGetChildrenCallback,
+                null);
+    }
+
+    AsyncCallback.ChildrenCallback workerGetChildrenCallback = new AsyncCallback.ChildrenCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, List<String> children) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    getWorkers();
+                    break;
+                case OK:
+                    LOG.info("Succefully got the list of children: "+children.size()+ " workers");
+                    reassignAndSet(children);
+                    break;
+                default:
+                    LOG.error("getChildren failed: "+ KeeperException.create(KeeperException.Code.get(rc),path));
+            }
+        }
+    };
+
+    ChildrenCache workersCache;
+    void reassignAndSet(List<String> children){
+        List<String> toProcess;
+        if(workersCache == null){
+            workersCache = new ChildrenCache(children);
+            toProcess = null;
+        }else{
+            LOG.info("Removing and Setting");
+            toProcess = workersCache.removeAndSet(children);
+        }
+        if(toProcess != null){
+            for(String s : toProcess){
+                // TODO: getAbsentWorkersTasks
+            }
+        }
+    }
+
+    Watcher tasksChangeWatcher = new Watcher() {
+        @Override
+        public void process(WatchedEvent watchedEvent) {
+            if (watchedEvent.getType()== Event.EventType.NodeChildrenChanged){
+                assert "/tasks".equals(watchedEvent.getPath());
+                getTasks();
+            }
+        }
+    };
+
+    ChildrenCache tasksCache;
+    void getTasks(){
+        zk.getChildren("/tasks",
+                tasksChangeWatcher,
+                getTasksCallback,
+                null
+        );
+    }
+
+    AsyncCallback.ChildrenCallback getTasksCallback = new AsyncCallback.ChildrenCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, List<String> tasks) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    getTasks();
+                    break;
+                case OK:
+                    List<String> toProcess;
+                    if(tasksCache == null){
+                        tasksCache = new ChildrenCache(tasks);
+                        toProcess = tasks;
+                    }else{
+                        toProcess = tasksCache.addAndSet(tasks);
+                    }
+                    if(toProcess != null){
+                        assignTasks(toProcess);
+                    }
+                default:
+                    LOG.error("getChildren failed: " + KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
+
+    void assignTasks(List<String> tasks){
+        for(String task: tasks){
+            getTaskData(task);
+        }
+    }
+
+    void getTaskData(String task){
+        zk.getData("/tasks/"+task,
+                false,
+                getDataCallback,
+                task);
+    }
+
+    AsyncCallback.DataCallback getDataCallback = new AsyncCallback.DataCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    getTaskData((String)ctx);
+                    break;
+                case OK:
+                    List<String> workers = workersCache.getList();
+                    String choosenWorker = workers.get(random.nextInt(workers.size()));
+                    String assignmentPath = "/workers/"+choosenWorker+"/"+(String)ctx;
+                    LOG.info("Assigned path: "+ assignmentPath);
+                    createAssignmentPath(assignmentPath, data);
+                    break;
+                default:
+                    LOG.error("getData failed: "+ KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
+
+    void createAssignmentPath(String path, byte[] data){
+        zk.create(path,
+                data,
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT,
+                assignTaskCallback,
+                data);
+    }
+
+    AsyncCallback.StringCallback assignTaskCallback = new AsyncCallback.StringCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx, String name) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    createAssignmentPath(path, (byte[])ctx);
+                    break;
+                case OK:
+                    LOG.info("Task assigned successfullt: "+ name);
+                    deleteTask(name.substring(name.lastIndexOf("/")+1));
+                    break;
+                case NODEEXISTS:
+                    LOG.warn("task Already assigned");
+                    break;
+                default:
+                    LOG.error("Something wrong happened while assigning task: " + KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
+
+    void deleteTask(String task){
+        zk.delete(task, -1, deleteTaskCallback, null);
+    }
+
+    AsyncCallback.VoidCallback deleteTaskCallback = new AsyncCallback.VoidCallback() {
+        @Override
+        public void processResult(int rc, String path, Object ctx) {
+            switch (KeeperException.Code.get(rc)){
+                case CONNECTIONLOSS:
+                    deleteTask((String)ctx);
+                    break;
+                case NONODE:
+                    LOG.info("Task is already deleted");
+                    break;
+                case OK:
+                    LOG.info("task is deleted successfully");
+                    break;
+                default:
+                    LOG.error("There was something wrong with deleting task:" + KeeperException.create(KeeperException.Code.get(rc), path));
+            }
+        }
+    };
 }
